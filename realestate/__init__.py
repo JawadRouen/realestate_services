3362087af1b6a401bccf87ee9a3da40d8b0a1d72
JawadRouen <JawadRouen@users.noreply.github.com>
2017-07-06 08:48:47 +0200

    Add files via upload

--- realestate/__init__.py
+++ realestate/__init__.py
@@ -1,5 +1,6 @@
 from .gen_reicept import *
-
+from .gen_contract import *
 
 __all__ = []
 __all__ += gen_reicept.__all__
+__all__ += gen_contract.__all__
---
+++ realestate/gen_contract.py
@@ -0,0 +1,15 @@
+from flask import render_template
+#from weasyprint import HTML, CSS
+
+
+__all__ = ['contract']
+
+def contract(template_name, contract_info):
+    html_result = render_template(template_name, **contract_info)
+    return html_result
+
+
+
+
+
+    
